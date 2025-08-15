8c7f208994f62e4af73678c92dbb121ce5aded79
zumaichi <dzumaquero@gmail.com>
2025-08-15 19:39:04 +0200

    eliminar

---
+++ Bundling/webpack/webpack.config.js
@@ -0,0 +1,59 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+export default {
+  context: path.resolve(__dirname, "./src"),
+  entry: {
+    app: "./index.js",
+    vendorStyles: ["../node_modules/bootstrap/dist/css/bootstrap.css"],
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset/resource",
+      },
+      { 
+        test: /\.html$/,
+        loader: "html-loader",
+      }
+    ],
+  },
+  devServer: {
+    port: 8080,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./index.html",
+      scriptLoading: "blocking",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};

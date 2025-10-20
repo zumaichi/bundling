87aca3c712a15145fc4380deb9526045e9f49b59
zumaichi <dzumaquero@gmail.com>
2025-10-20 10:38:55 +0200

    fin

--- react/src/app.tsx
+++ react/src/app.tsx
@@ -8,6 +8,8 @@ import {
 import { LoginPage } from "./login";
 import { ListPage } from "./list";
 import { DetailPage } from "./detail";
+import { RickMortyPage } from "./rickmorty";
+import { RickMortyDetailPage } from "./rickmortyDetail";
 
 export const App = () => {
   return (
@@ -16,6 +18,8 @@ export const App = () => {
         <Route path="/" element={<LoginPage />} />
         <Route path="/list" element={<ListPage />} />
         <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="/rickmorty" element={<RickMortyPage />} />
+        <Route path="/rickmorty/:id" element={<RickMortyDetailPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
